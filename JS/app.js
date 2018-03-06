f877b31a1bc111949db5ee3d9b86d2a57026c826
Sam Porter Giffords <travelinsamp@gmail.com>
2018-03-06 15:57:59 -0800

    Saving after first successful prompt test

--- JS/app.js
+++ JS/app.js
@@ -6,36 +6,55 @@
 /*
 if(some condition is met) {
   do this
-} else {}
+} else if {
   do something different
+} else {
+  do a third thing
 }
 */
 
-var petQuestion = parseInt(prompt('How many pets do you think I have ?  Please type in a number.'));
 
-if(petQuestion === 2) {
-  alert('correct');
-}  {
-  alert('nope');
-}
+var test1 = prompt('do you see this');
+console.log ("Test 1 answer is " + test1);
 
-
-var myAge = prompt('Do you think I\'m older than 30 ?  Please enter "yes" or "no"').toLowerCase();
-//myAge = myAge.toLowerCase();
-
-// 'yes', 'YES', 'Y', 'y', "YeS, 'NO', 'no', 'No', 'nO', 'n', 'N', 'nope', 'nah', 'yep'
-
-if (myAge === 'yes' || 'Y' || "Yes") {
+/*
+var containTheobromine = prompt('Does chocolate contain theobromine?').toLowerCase();
+if (containTheobromine === 'yes' || 'Y' || "Yes") {
   alert('That is correct !');
 } else {
-  alert('No, I am older than 30 !');
+  alert('It actually does. =D');
 }
+  console.log ("You answered " + containTheobromine + " when I asked if chocolate contained theobromine.")
 
-if(first condition) {
-  do this;
-} else if(second condition) {
-   do this;
- } else {
-     do this;
-   }
+var rubyChocolate = prompt('Is there such a thing as ruby chocolate ?').toLowerCase();
+if (rubyChocolate === 'yes' || 'Y' || "Yes") {
+  alert('That is correct !');
+} else {
+  alert('It\'s very new and proprietary.');
+}
+  console.log ("You answered " + containTheobromine + " when I asked if chocolate contained theobromine.")
 
+var labsOfChocolate = prompt ('Are chocolate labs made of cocoa ?').toLowerCase();
+if (labsOfChocolate === 'no' || 'N' || "No") {
+  alert('That is correct !');
+} else {
+  alert('I hope not !  Chocolate is bad for dogs !');
+}
+  console.log ("You answered " + containTheobromine + " when I asked if chocolate contained theobromine.")
+
+var marsCandy = prompt ('Are M\&M\'s actually from Mars ?').toLowerCase();
+if (marsCandy === 'yes' || 'Y' || "Yes") {
+  alert('That is correct - the Mars family makes them.');
+} else {
+  alert('Well. . . sort of. . .');
+}
+  console.log ("You answered " + containTheobromine + " when I asked if chocolate contained theobromine.")
+
+var tiredOfChocolate = prompt('Are you tired of all these chocolate questions ?').toLowerCase();
+if (tiredOfChocolate === 'yes' || 'Y' || "Yes") {
+  alert('Awww.  I\'m not !');
+} else {
+  alert('Me, neither !');
+}
+console.log ("You answered " + containTheobromine + " when I asked if chocolate contained theobromine.")
+*/
