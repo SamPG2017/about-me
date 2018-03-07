13dfd67cd826465ed8e9d2a30e274fd4bf0de739
Sam Porter Giffords <travelinsamp@gmail.com>
2018-03-06 16:21:22 -0800

    Added a commented set of questions for testing alt answers

--- JS/app.js
+++ JS/app.js
@@ -59,8 +59,46 @@ if (tiredOfChocolate === 'yes') {
 }
 console.log ("You answered " + containTheobromine + " when I asked if you were tired of chocolate questions.")
 
+/* -- Set up another set of questions to test alternate answers for functionality
+var containTheobromine = prompt('Does chocolate contain theobromine?').toLowerCase();
+if (containTheobromine === 'yes') {
+  alert('That is correct !');
+} else {
+  alert('It actually does. =D');
+}
+  console.log ("You answered " + containTheobromine + " when I asked if chocolate contained theobromine.")
 
-/*
 
+var rubyChocolate = prompt('Is there such a thing as ruby chocolate ?').toLowerCase();
+if (rubyChocolate === 'yes') {
+  alert('That is correct !');
+} else {
+  alert('It\'s very new and proprietary.');
+}
+  console.log ("You answered " + containTheobromine + " when I asked if ruby chocolate exists.")
+
+var labsOfChocolate = prompt ('Are chocolate labs made of cocoa ?').toLowerCase();
+if (labsOfChocolate === 'no') {
+  alert('That is correct !');
+} else {
+  alert('I hope not !  Chocolate is bad for dogs !');
+}
+  console.log ("You answered " + containTheobromine + " when I asked if chocolate labs are made of cocoa.")
+
+var marsCandy = prompt ('Are M\&M\'s actually from Mars ?').toLowerCase();
+if (marsCandy === 'yes') {
+  alert('That is correct - the Mars family makes them.');
+} else {
+  alert('Well. . . sort of. . .');
+}
+  console.log ("You answered " + containTheobromine + " when I asked if M\&M\'s are actually from Mars.")
+
+var tiredOfChocolate = prompt('Are you tired of all these chocolate questions ?').toLowerCase();
+if (tiredOfChocolate === 'yes') {
+  alert('Awww.  I\'m not !');
+} else {
+  alert('Me, neither !');
+}
+console.log ("You answered " + containTheobromine + " when I asked if you were tired of chocolate questions.")
 */
 
